6853eaba6dd21d59a75704cccbd5e9cfaa2ada95
Sehaj Naangal <snaangal@uwo.ca>
2023-02-06 21:05:10 -0500

    delete ProjectDetails component

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import './projects.css';
+import './project.css';
 
 export default function Projects(props) {
     return (
         <div className='projects'>
             <button
             className='projects-button'
-            onClick={() => props.handleClick(props.details)}
+            onClick={() => props.handleClick(props.details.id)}
             >
                 {props.details.name}
             </button>
+
+            <div>
+                {}
+            </div>
         </div>
     )
 }
