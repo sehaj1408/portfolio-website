5c87dab6b807ca517b4e0d0e4ecff0aad7b252b4
Sehaj Naangal <snaangal@uwo.ca>
2023-02-07 13:33:08 -0500

    styling for about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
             <h1 className='about-name'>Hi, I'm Sehaj</h1>
             <p className='about-p1'>I'm a third-year Software Engineering student at Western University interested in areas of software engineering like full stack development, 
                 databases, continous integration, problem-solving, communication, and building high-quality applications using different programming 
-                languages. In my spare time, I enjoy playing the piano, going to the gym, playing tennis, and socializing.</p>            
+                languages. In my spare time, I enjoy playing the piano, going to the gym, and playing tennis.</p>            
         </div>
     )
 }
