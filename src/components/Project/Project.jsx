714d46c9fd343bb52ae4b78b4cd3fb2d8e81c389
Sehaj Naangal <snaangal@uwo.ca>
2023-02-07 17:49:45 -0500

    display image for each project

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,31 +2,38 @@ import React from 'react';
 import './project.css';
 
 export default function Projects(props) {
-    return (
-        <div className='projects' id='projects'>
-
-            <button
-            className='projects-button'
-            onClick={() => props.handleClick(props.details.id)}
-            >
-                {props.details.name}
-            </button>
-
-            <div
-            style={{display: props.showDetails ? 'block' : 'none'}}
-            >
-
-                <h3>{props.details.description}</h3>
-
-                <p>{props.details.skills}</p>
-
-                <a 
-                href={props.details.github}
-                target='_blank'
+    const imagePath = props.details.image;
+ 
+    return ( 
+        <div className='projects-container'>
+            <h2 className='projects-title'>What I've worked on and my experiences</h2>
+            <div className='projects' id='projects'>
+                <img 
+                src={require(`../../images/${imagePath}`)} 
+                alt="Image" 
+                className='projects-image'
+                />
+                <button
+                className='projects-button'
+                onClick={() => props.handleClick(props.details.id)}
                 >
-                {props.details.github && 'Github'}
-                </a>
-                
+                    {props.details.name}
+                </button>
+
+                <div
+                style={{display: props.showDetails ? 'block' : 'none'}}
+                className='project-details'
+                >
+                    <h3 className='details-description'>{props.details.description}</h3>
+                    <p className='details-skills'>{props.details.skills}</p>
+                    <a 
+                    href={props.details.github}
+                    target='_blank'
+                    className='details-github'
+                    >
+                    {props.details.github && 'Github'}
+                    </a>
+                </div>
             </div>
         </div>
     )
