10c5f8cffecf9241b29457057a0cdffa7c516c5a
Sehaj Naangal <snaangal@uwo.ca>
2023-02-09 14:09:43 -0500

    animation to about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,17 +5,29 @@ import { motion } from 'framer-motion';
 export default function Navbar() {
     return (
         <motion.div 
-        
         className='about' 
         id='about'
         >
-            <h1 className='about-name'>Hi, I'm Sehaj</h1>
-            <p className='about-p1'>I'm a third-year Software Engineering student 
+            <motion.h1 
+            className='about-name'
+            animate={{ y: -40 }}
+            transition={{ duration: 1 }}
+            >
+                Hi, I'm Sehaj
+            </motion.h1>
+            <motion.p 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+            className='about-p1'
+            >
+                I'm a third-year Software Engineering student 
             at Western University interested in areas of software engineering 
             like web development, iOS development, databases, cloud computing, 
             and building high-quality applications using different programming 
             languages. In my spare time, I enjoy playing the piano, 
-            going to the gym, and playing tennis.</p>
+            going to the gym, and playing tennis.
+            </motion.p>
         </motion.div>
     )
 }
