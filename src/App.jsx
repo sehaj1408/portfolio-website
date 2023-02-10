c2c2254209f202b361edcba59597714f47aca7bd
Sehaj Naangal <snaangal@uwo.ca>
2023-02-10 14:52:42 -0500

    set size using vh for skills and chang egrid minmax

--- src/App.jsx
+++ src/App.jsx
@@ -71,18 +71,18 @@ export default function App() {
     }) 
 
     const skillsArr = [
-        {name: 'Javascript', padding: '0px', height: '5.2vh', width: '8.5vh', image: 'javascriptImage.png'},
-        {name: 'HTML', padding: '0.02vw', height: '11.2vw', width: '11vw', image: 'htmlImage.png'},
-        {name: 'CSS', padding: '0.05vw', height: '11.1vw', width: '8vw', image: 'cssImage.png'},
-        {name: 'Swift', padding: '0.45vw', height: '10.3vw', width: '10.2vw', image: 'swiftImage.png'},
-        {name: 'SQL', padding: '0.3vw', height: '8.5vw', width: '18vw', image: 'sqlImage.png'},
-        {name: 'Java', padding: '0.12vw', height: '9vw', width: '7vw', image: 'javaImage.png'},
-        {name: 'React', padding: '0.55vw', height: '8.5vw', width: '9.8vw', image: 'reactImage.png'},
-        {name: 'Github', padding: '0.37vw', height: '9vw', width: '9vw', image: 'githubImage.png'},
-        {name: 'Node.js', padding: '0.8vw', height: '7vw', width: '12vw', image: 'nodejsImage.png'},
-        {name: 'Express', padding: '1.24vw', height: '6.5vw', width: '19vw', image: 'expressImage.png'},
-        {name: 'REST API', padding: '0.7vw', height: '8vw', width: '9.4vw', image: 'restapiImage.png'},
-        {name: 'AWS', padding: '1.1vw', height: '7.3vw', width: '11vw', image: 'awsImage.png'},
+        {name: 'Javascript', padding: '0px', height: '7.2vh', width: '12vh', image: 'javascriptImage.png'},
+        {name: 'HTML', padding: '0.02vw', height: '7.2vh', width: '7.6vh', image: 'htmlImage.png'},
+        {name: 'CSS', padding: '0.05vw', height: '7.2vh', width: '5.5vh', image: 'cssImage.png'},
+        {name: 'Swift', padding: '0.45vw', height: '6.6vh', width: '6.6vh', image: 'swiftImage.png'},
+        {name: 'SQL', padding: '0.3vw', height: '5vh', width: '11vh', image: 'sqlImage.png'},
+        {name: 'Java', padding: '0.12vw', height: '5.3vh', width: '4.2vh', image: 'javaImage.png'},
+        {name: 'React', padding: '0.55vw', height: '4.9vh', width: '6vh', image: 'reactImage.png'},
+        {name: 'Github', padding: '0.37vw', height: '5.5vh', width: '5.5vh', image: 'githubImage.png'},
+        {name: 'Node.js', padding: '0.8vw', height: '5vh', width: '8.5vh', image: 'nodejsImage.png'},
+        {name: 'Express', padding: '1.24vw', height: '4.5vh', width: '14vh', image: 'expressImage.png'},
+        {name: 'REST API', padding: '0.7vw', height: '5.5vh', width: '6.5vh', image: 'restapiImage.png'},
+        {name: 'AWS', padding: '1.1vw', height: '5.2vh', width: '8.4vh', image: 'awsImage.png'},
     ]
 
     const skillsElements = skillsArr.map((skill) => {
@@ -154,7 +154,7 @@ export default function App() {
             </div>
             <div className='contact-container' id='contact-container'>
                 <div className='contact-container-navbar' id='contact-container-navbar'></div>
-                <h2 data-aos='fade-down' data-aos-anchor-placement='center-bottom' className='contact-header'>Get in Touch</h2>
+                <h2 data-aos='fade-down' data-aos-anchor-placement='bottom-bottom' className='contact-header'>Get in Touch</h2>
                 <Contact 
                     name = 'Sehaj Naangal'
                     email = 'sehajnaangal@gmail.com'
