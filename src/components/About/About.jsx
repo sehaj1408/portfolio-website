7a1db3bfc68d492bfe8cb7d456f1eadb9de2d26b
Sehaj Naangal <snaangal@uwo.ca>
2023-03-27 19:30:16 -0400

    move about content to left half and add project button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,34 +3,65 @@ import './about.css';
 import { motion } from 'framer-motion';
 
 export default function Navbar() {
+    function scrollToComponent(id) {
+        const component = document.getElementById(id);
+        window.scrollTo({
+            top:component.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+
     return (
         <motion.div 
-        className='about' 
-        id='about'
+            className='about' 
+            id='about'
         >
             <motion.h1 
-            className='about-name'
-            style={{ paddingTop: '40px' }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, y: -40 }}
-            transition={{ duration: 0.8 }}
+                className='about-name'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, y: -40 }}
+                transition={{ duration: 0.8 }}
             >
                 Hi, I'm Sehaj
             </motion.h1>
-            <motion.p 
-            style={{ marginTop: '-10px' }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
-            className='about-p1'
+
+            <motion.h3
+                className='about-title'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
             >
-                I'm a third-year Software Engineering student 
+                Software Engineering Student
+            </motion.h3>
+
+            <motion.p 
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.7 }}
+                className='about-p1'
+            >
+            I'm a third-year Software Engineering student 
             at Western University interested in areas of software engineering 
             like web development, iOS development, databases, cloud computing, 
             and building high-quality applications using different programming 
             languages. In my spare time, I enjoy playing the piano, 
             going to the gym, and playing tennis.
             </motion.p>
+
+            <motion.button
+                className='about-projects'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.9 }}
+                onClick={() => scrollToComponent('projects-container-navbar')}
+            >
+                Projects 
+                <img 
+                    src={require("../../images/downArrow.png")} 
+                    alt="downArrow" 
+                    style={{ height: '18px', width: '18px', paddingLeft: '3px' }}
+                />
+            </motion.button>
         </motion.div>
     )
 }
