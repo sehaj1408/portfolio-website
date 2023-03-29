ab758fe18880f70c180f86860dc8b6d93d83547a
Sehaj Naangal <snaangal@uwo.ca>
2023-03-29 19:53:12 -0400

    fix bootstrap navbar css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,39 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './navbar.css';
+import { motion } from 'framer-motion';
 
 export default function MyNavbar() {
-  return (
-    <Navbar bg="dark" expand="lg" variant='dark' className='nav'>
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    function scrollToComponent(id) {
+        const component = document.getElementById(id);
+        window.scrollTo({
+            top:component.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+    
+    return (
+        <Navbar expand="lg" className='nav'>
+            <Container className='nav-container'>
+                <button onClick={() => scrollToComponent('about')} className='nav-brand'>Sehaj Naangal</button>
+                <Navbar.Toggle aria-controls="navbarSupportedContent" className='nav-hamburger-icon'/>
+                <Navbar.Collapse id="basic-navbar-nav" className='nav-hamburger-menu'>
+                    <Nav className='nav-buttons'>
+                        <button onClick={() => scrollToComponent('about')}>ABOUT</button>
+                        <button onClick={() => scrollToComponent('projects-container-navbar')}>PROJECTS</button>
+                        <button onClick={() => scrollToComponent('skills-container-navbar')}>TECHNOLOGIES</button>
+                        <button onClick={() => scrollToComponent('education-container-navbar')}>EDUCATION</button>
+                        <button onClick={() => window.open('https://drive.google.com/file/d/1zc82GDtKw-6w2-fLYeU0FF52gVg-VVjC/view?usp=share_link', '_blank')}>RESUME</button>
+                        <button onClick={() => scrollToComponent('contact-container-navbar')}>CONTACT</button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
+
+
 
 
 // import React, { useEffect } from 'react';
